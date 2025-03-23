264b25fecbe0883e28d2099f9a32cf63ff20bd0e
farhad <farhad@novalab.se>
2025-03-23 14:08:30 +0100

    create functions to interact with twitter

---
+++ src/services/twitter.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { config } from '../config';
+
+const API = 'https://api.twitter.com/2';
+const headers = { Authorization: `Bearer ${config.bearerToken}` };
+
+export const getMyUserInfo = async () => {
+  const res = await axios.get(`${API}/users/me`, { headers });
+  return res.data.data;
+};
+
+export const getMyReplies = async (username: string) => {
+  const res = await axios.get(`${API}/tweets/search/recent`, {
+    headers,
+    params: {
+      query: `from:${username}`,
+      "tweet.fields": "conversation_id,created_at,text"
+    }
+  });
+  return res.data.data || [];
+};
+
+export const getConversationReplies = async (conversationId: string) => {
+  const res = await axios.get(`${API}/tweets/search/recent`, {
+    headers,
+    params: {
+      query: `conversation_id:${conversationId}`,
+      "tweet.fields": "author_id,created_at"
+    }
+  });
+  return res.data.data || [];
+};
+
+export const muteOrBlockUser = async (myUserId: string, targetUserId: string, action: string) => {
+  const endpoint = action === 'mute'
+    ? `${API}/users/${myUserId}/muting`
+    : `${API}/users/${myUserId}/blocking`;
+    
+  return axios.post(endpoint, { target_user_id: targetUserId }, { headers });
+};
